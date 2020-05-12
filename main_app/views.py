39bcccecdcdc1e0acbf68ce638e1f188133ebf47
Bobby Coleman <colemankidding@gmail.com>
2020-05-12 15:58:13 -0700

    Impliment print two words from api to console

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import requests 
+# from random_word import RandomWords
 
 # Create your views here.
+
+r = requests.get("https://random-word-api.herokuapp.com/word?number=2")
+arr = r.json()
+
+def get_words(request):
+  print(arr[0])
+
+get_words()
+
+# def get_words(req):
+#   print(r)
+# r = RandomWords()
+
+# def get_words():
+  # print(r.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10))
+
+# r = requests.get("https://random-word-api.herokuapp.com/word?number=2")
+# if r.status_code != 200:
+#     # This means something went wrong.
+#     raise ApiError('GET /tasks/ {}'.format(r.status_code))
+# for todo_item in r.json():
+#     print('{} {}'.format(todo_item['id'], todo_item['summary'])
+
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def words(request):
   return render(request, 'twowords/index.html')
 
--- twoWords/settings.py
+++ twoWords/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
+    'random_word',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
