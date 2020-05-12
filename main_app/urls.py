06c35178572c273c413be9a3995a9218bae7dd37
Bobby Coleman <colemankidding@gmail.com>
2020-05-11 22:37:25 -0700

    impliment signup and display username on left side of nav bar

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.about, name='about'),
     path('words/', views.words, name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,3 +9,18 @@ def about(request):
 
 def words(request):
   return render(request, 'twowords/index.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
