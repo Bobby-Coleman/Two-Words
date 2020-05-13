f4140893bbdebf87aa0e0ea2dad64960e1f81962
Bobby Coleman <colemankidding@gmail.com>
2020-05-12 20:09:01 -0700

    attempting to insert api word data into model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.about, name='about'),
     path('words/', views.words, name='index'),
+    path('words/list', views.words_list, name='list'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,27 +6,42 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 import requests 
 import json
 # from random_word import RandomWords
+from . models import Word
 
 @login_required
 def words(request):
   r = requests.get("https://random-word-api.herokuapp.com/word?number=2").json()
+
   word_one = r[0]
   word_two = r[1]
-  try:
-    words = json.loads(r.content)
 
-  except Exception as e:
-    words = "Error data not loading"
+  if request.method == "GET":
+    try:
+      words = json.loads(r.content)
 
-  words = {
-    'word_one' : word_one,
-    'word_two' : word_two,
-  }
+    except Exception as e:
+      words = "Error data not loading"
+
+      words = {
+      'word_one' : word_one,
+      'word_two' : word_two,
+     }
+
+  if request.method == 'POST':
+      Word.objects.create(word_one = word_one, word_two = word_two)
 
   context = {'words' : words }
 
   return render (request, 'twowords/index.html', context)
 
+
+@login_required
+def words_list(request):
+  # words = Word.objects.filter(user=request.user)
+  # words = Word.objects.all()
+  return render(request, 'twowords/list.html', )
+
+# { 'words': words }
 
 def about(request):
   return render(request, 'about.html')
