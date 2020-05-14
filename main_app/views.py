48d71834f3366b9bd5cb84a41761fba088184edf
Bobby Coleman <colemankidding@gmail.com>
2020-05-14 16:38:08 -0700

    impliment list, filter for words with comments

--- main_app/views.py
+++ main_app/views.py
@@ -5,35 +5,22 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import requests 
 import json
+from itertools import chain
 from .forms import CommentForm
-# from random_word import RandomWords
 from . models import Word, Comment
 
 @login_required
 def words(request):
+  #retrieve two words from api
   r = requests.get("https://random-word-api.herokuapp.com/word?number=2").json()
-  # print(r)
-  # print(type(r))
-  # print(x)
 
-  word_one = json.dumps(r[0])
-  word_two = json.dumps(r[1])
-  # print(request.method)
-  # try:
-  #   words = json.loads(r.content)
+  #isolate each word
+  word_one = r[0]
+  word_two = r[1]
 
-  # except Exception as e:
-  #   words = "Error data not loading"
-
-  # words = {
-  #     'word_one' : word_one,
-  #     'word_two' : word_two,
-  # }
-
-  # print(request.user)
+  #save word pair to the database
   new_word = Word.objects.create(word_one = word_one, word_two = word_two, user_id = request.user.id)
-  # print(new_word)
-  # comment_form = CommentForm()
+
   context = {
     'new_word' : new_word,
   }
@@ -42,18 +29,16 @@ def words(request):
 
 @login_required
 def words_list(request):
-  #change to words that are saved by user
-  words = Word.objects.filter(user=request.user)
 
-  # words = Word.objects.all()
+  #query for words that have been commented on by the user and order them by most recently created
+  user_words = Word.objects.filter(user=request.user)
+  words = user_words.exclude(comment__content = None).order_by('-id')
 
   context = {
     'words' : words,
-  #  'comments' : comments,
   }
   return render(request, 'twowords/list.html', context)
 
-# { 'words': words }
 
 def about(request):
   return render(request, 'about.html')
